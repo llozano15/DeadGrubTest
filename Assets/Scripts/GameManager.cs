f23162c0fa443656f8af984598d4d25b745b9600
NotWinni <wwang53@pratt.edu>
2026-03-02 19:13:38 -0500

    Set GameManager to Singleton Instance

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,29 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {   
+    public static GameManager Instance; //Singleton instance to allow other scripts to easily access the GameManager
+    
+    [Header("FoodTags")]
     public Food[] foodItems; // Array to hold references to all food items in the scene
     private string[] houseTags = { "House_One", "House_Two", "House_Three", "House_Four", "House_Five", "House_Six", "House_Seven", "House_Eight", "House_Nine", "House_Ten", "House_Eleven", "House_Twelve"};
+
+    //Method is called only one time when script is loaded before Start()
+    void Awake()
+    {
+        //Singleton Pattern
+        //Makes sure only one GameManager exists
+        if (Instance == null)
+        {
+            Instance = this;
+            Debug.Log("GameManager Instance Set");
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
